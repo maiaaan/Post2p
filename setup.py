d6bf8c47478fbc7681edc600a42d4a896c9f66cc
maiaaan <72934667+maiaaan@users.noreply.github.com>
2024-04-05 17:30:34 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='Post2p',
+   version='1.0',
+   description='An integrated software to analyze the results of calcium imaging and behavioral data',
+   author='Faezeh Rabbani',
+   packages=['Post2p']  #same as name
+)
