daf99f3404f60a5f9a4312ec19ebff3705a94733
Faezeh Rabbani <101398285+faezehrabbani97@users.noreply.github.com>
2024-04-09 12:16:49 +0200

    Create setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+setup(
+    name='Post2p',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/faezehrabbani97/Post2p',
+    license='MIT',
+    author='Faezeh Rabbani',
+    author_email='faezeh.rabbani97@gmail.com',
+    description='Description of your package',
+    install_requires=[requirements],
+)
